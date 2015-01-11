5dd6f62beecf6bb21b9f2d6e766d3f81e4765b46
Kevin Wu <kevinpingwu2@gmail.com>
2015-01-11 15:40:45 +1100

    Milestone: Added Spring Boot Actuator to make app production-ready

--- src/main/java/com/yang/cicada/CicadaServiceApplication.java
+++ src/main/java/com/yang/cicada/CicadaServiceApplication.java
@@ -1,7 +1,10 @@
 package com.yang.cicada;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,21 @@ import org.springframework.context.annotation.Configuration;
 public class CicadaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CicadaServiceApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(CicadaServiceApplication.class, args);
+		
+		/*
+		 * When Actuator is added to pom.xml, the beans can be inspected at endpoint http://localhost:8080/beans
+		 * 
+		 * See http://docs.spring.io/spring-boot/docs/1.1.10.RELEASE/reference/htmlsingle/#production-ready
+		 * 
+		 * *** Make sure these endpoints are guarded for SECURITY! ***
+		 */
+//		System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//        String[] beanNames = ctx.getBeanDefinitionNames();
+//        Arrays.sort(beanNames);
+//        for (String beanName : beanNames) {
+//            System.out.println("***bean=" + beanName);
+//        }
 	}
 }
